167770b45663731251ecee50e6a4856e56839df0
DAFTCCC <daftccc@gmail.com>
2026-09-24 14:57:23 -1000

    Enable FieldReady public backend

--- backend-config.js
+++ backend-config.js
@@ -1,7 +1,7 @@
 window.FIELDREADY_BACKEND = Object.freeze({
-  enabled: false,
-  url: 'https://FIELDREADY-BACKEND.example.com',
-  anonKey: 'REPLACE_WITH_FIELDREADY_ANON_KEY',
+  enabled: true,
+  url: 'https://raps-supabase.asuscomm.com/fieldready',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzkwMjI3MjI5LCJleHAiOjE5NDc5MDcyMjl9.5-zqbGxF9Dy_nND6Whg5rwQyWD0hKgRN5a38xbflpWA',
   auth: Object.freeze({
     sessionKey: 'FIELDREADY_SUPABASE_SESSION_V1'
   }),
